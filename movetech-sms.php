a3bcc91effafc06ec7b1abbb79a9aaf979feec84
movetechke <kumakibaki@gmail.com>
2018-01-08 21:28:34 +0300

    Add files via upload
    Our SACCO Management System is a microfinance software by Movetech Solutions Ltd which is capable of handling member creation and management, register management including issuance of share certificates. The system handles savings computation and management as well as Loans processing from applications which includes ongoing loan management .
    
    Our Sacco ERP Software helps Saccos automate all operations from members registration, loans processing, shares and dividends, Front office administration to Financials with Bulk SMS , HRM and Payroll integrated.
    
    To help in installing https://www.movetechsolutions.com/sacco-management-software/
    
    Also we are coming up with developers and documentation on www.movesacco.co.ke
    
    To install prepare ypur computer or server with Oracle Database, Tomcat Server and SDK JRE.

---
+++ movetech-sms.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  Plugin Name: Movetech Bulk SMS
+  Plugin URI: http://bulksms.movetechsolutions.com/
+  Description: Movetech Bulk SMS to send bulk sms plugin using the wordpress website.
+  Version: 1.0
+  Author: Evans Mwiti
+  Author URI: http://mwiti.com
+  License: GPLv2+
+  Text Domain: movetech-sms
+*/
+?>
